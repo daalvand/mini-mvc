165b59a4b8bc705825203712f40a9a3b31eb7374
Mehdi <mdaalvand@gmail.com>
2023-04-12 17:19:25 +0330

    update index and create App container

---
+++ core/App.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Core;
+
+
+use Core\Contracts\App as AppContract;
+
+class App implements AppContract
+{
+    protected static self $instance;
+    protected array       $callbacks; // services that can instantiated
+    protected array       $instantiated;// instantiated services
+
+    public function __construct(protected string $basePath, protected array $configs)
+    {
+        static::$instance = $this;
+    }
+
+    public static function getInstance(): static
+    {
+        return static::$instance;
+    }
+
+    public function run(): void
+    {
+        echo 'hello world!';
+    }
+
+    public function set(string $contract, callable|object $callable): void
+    {
+        unset($this->instantiated[$contract]);
+        if (is_callable($callable)) {
+            $this->callbacks[$contract] = $callable;
+        } else {
+            $this->instantiated[$contract] = $callable;
+        }
+    }
+
+    public function get(string $contract): mixed
+    {
+        if (isset($this->instantiated[$contract])) {
+            return $this->instantiated[$contract];
+        }
+        if (isset($this->callbacks[$contract])) {
+            $this->instantiated[$contract] = $this->callbacks[$contract]($this);
+            return $this->instantiated[$contract];
+        }
+        return new $contract;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo 'hello world';
+use Core\App;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$config = require __DIR__ . '/../config.php';
+$app    = new App(dirname(__DIR__), $config);
+
+$app->run();
