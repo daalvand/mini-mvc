f0f2c8a2b8bcb5377b675c4ea64fe719e14d3180
Mehdi <mdaalvand@gmail.com>
2023-04-23 11:16:34 +0330

    add new helpers

--- app/Helpers/app.php
+++ app/Helpers/app.php
@@ -2,11 +2,15 @@
 
 use Core\App;
 use Core\Contracts\App as AppContract;
+use Core\Contracts\AuthManager;
 use Core\Contracts\DB\Database;
 use Core\Contracts\DB\Migrator;
 use Core\Contracts\DB\QueryBuilder;
 use Core\Contracts\DB\Schema;
+use Core\Contracts\Http\Request;
+use Core\Contracts\Http\Response;
 use Core\Contracts\Router;
+use Core\Contracts\Session;
 use Core\Contracts\View;
 
 function app(): AppContract
@@ -42,4 +46,29 @@ function render_view(string $view, array $data = []): string
 function query_builder(): QueryBuilder
 {
     return app()->get(QueryBuilder::class);
+}
+
+function view(): View
+{
+    return app()->get(View::class);
+}
+
+function session(): Session
+{
+    return app()->get(Session::class);
+}
+
+function response(): Response
+{
+    return app()->get(Response::class);
+}
+
+function request(): Request
+{
+    return app()->get(Request::class);
+}
+
+function auth(): AuthManager
+{
+    return app()->get(AuthManager::class);
 }
