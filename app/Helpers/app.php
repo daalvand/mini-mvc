08d6f9effa8944cefef7f929533807e5cb6189ee
Mehdi <mdaalvand@gmail.com>
2023-04-26 23:48:18 +0330

    add new helper method getallheaders

--- app/Helpers/app.php
+++ app/Helpers/app.php
@@ -77,3 +77,16 @@ function csrf_token(): string
 {
     return session()->csrfToken();
 }
+
+if (!function_exists('getallheaders')) {
+    function getallheaders(): array
+    {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (str_starts_with($name, 'HTTP_')) {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
